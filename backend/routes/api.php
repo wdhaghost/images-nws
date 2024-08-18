7c507782c97eb7558fe4d7bdb5956e38b332d4e5
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-08-18 21:33:42 +0200

    route api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,5 +10,7 @@ Route::post('/login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
-
